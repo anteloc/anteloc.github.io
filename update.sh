9cfcad2eacf1a005580dae542d0ecf7e7377bc9b
anteloc <6914284+anteloc@users.noreply.github.com>
2025-04-21 00:26:44 +0200

    auto updated

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo "INFO: Updating fusion-reader on anteloc.github.io"
+
+to_delete=$(ls -1 | grep -vE 'update.sh|README.md|LICENSE|.git' | tr '\n' ' ')
+
+echo "INFO: Deleting the following files: $to_delete"
+rm -rf $to_delete
+
 cp -r ../fusion-reader/dist/* .
 git add . && git commit -m "auto updated" # && git push
