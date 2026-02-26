afcdd7ccb3b66c53b783f26eb89ffdc77d8b0465
anteloc <6914284+anteloc@users.noreply.github.com>
2026-02-26 21:25:49 +0100

    Deploy ModelScope XR viewer for web and VR/MR

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "INFO: Updating fusion-reader on anteloc.github.io"
+
+to_delete=$(ls -1 | grep -vE 'update.sh|README.md|LICENSE|.git' | tr '\n' ' ')
+
+echo "INFO: Deleting the following files: $to_delete"
+rm -rf $to_delete
+
+cd ../fusion-reader
+npm run build
+
+cd -
+
+cp -r ../fusion-reader/dist/* .
+git add . && git commit -m "auto updated" && git push
